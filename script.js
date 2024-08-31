b3567e0dc3f23c2cb9bbd95b62b6f5891a91c1be
sanma91 <mattiasanti1991@gmail.com>
2024-08-31 10:35:13 +0200

    Add dataset field to the book status so it gets removed from array when the "Remove" button is clicked

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const myLibrary = [];
 console.log(myLibrary);
 console.log(myLibrary.length);
 
-function Book(title, author, pages, id) {
+function Book(title, author, pages) {
     this.title = title
     this.author = author;
     this.pages = pages;
@@ -21,19 +21,20 @@ function Book(title, author, pages, id) {
             }
         }
     };
-    this.id = id;
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book)
 }
 
+let indexNumber = 0;
+
 submit.addEventListener("click", (event) => {
     const book = new Book(title, author, pages);
     if(book.title.value === "" || book.author.value === "" || book.pages.value === "") {
         alert("Complete all fields");
         return "Complete all fields"
-    }
+    }-
     addBookToLibrary(book);
     console.log(myLibrary);
     console.log(myLibrary.length);
@@ -50,15 +51,20 @@ submit.addEventListener("click", (event) => {
     tdPages.textContent = book.pages.value;
     const tdRead = document.createElement("td");
     row.appendChild(tdRead);
-    tdRead.textContent = book.isRead();
+    const tdReadButton = document.createElement("button");
+    tdRead.appendChild(tdReadButton);
+    tdReadButton.textContent = book.isRead();
+    tdReadButton.setAttribute("style", "height: 50%; width; 50%; background-color: wheat; border: 1px solid black");
     const button = document.createElement("button");
-    button.setAttribute("style", "position:relative; right: -1rem; bottom: -2rem")
-    row.appendChild(button)
+    button.setAttribute("style", "position:relative; right: -1rem; bottom: -2.25rem");
+    button.dataset.indexNumber = ++indexNumber;
+    row.appendChild(button);
     button.textContent = "Remove";
     button.addEventListener("click", () => {
         row.textContent = "";
-        console.log(myLibrary);
-        console.log(myLibrary.length)
+        const index = myLibrary.indexOf(button.dataset.indexNumber);
+        const removeBook = myLibrary.splice(index, 1);
+        return removeBook
     })
     event.preventDefault()
 })
